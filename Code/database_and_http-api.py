61db0bd5edd2c455bac32f955bca1c9f0dbbf0e2
ChristianStraub <straub.christi-it23@it.dhbw-ravensburg.de>
2025-02-17 11:50:26 +0100

    HTTPS zu HTTP

--- Code/database_and_http-api.py
+++ Code/database_and_http-api.py
@@ -152,4 +152,4 @@ def get_messpunkte(fahrt_id):
 # Server starten
 if __name__ == "__main__":
     init_db()
-    app.run(host="0.0.0.0", port=5000, ssl_context=("cert.pem", "key.pem"))
+    app.run(host="0.0.0.0", port=5000)
